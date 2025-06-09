8ca3ef182287f69ce67217ced94909b93b34ebd3
Cahya Aji Permana <cahyaajipermana@macbook-air.local>
2025-06-09 11:04:46 +0700

    add allowed host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
         changeOrigin: true,
         secure: false
       }
-    }
+    },
+    allowedHosts: [
+      "qa-automation.cahyaap.my.id",
+    ],
   }
 })
